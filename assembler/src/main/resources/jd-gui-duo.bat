18e87868649d6d586de15cd455024c2906e53825
nbauma109 <nbauma109@users.noreply.github.com>
2022-01-26 17:31:43 +0100

    Fix .bat file

--- assembler/src/main/resources/jd-gui-duo.bat
+++ assembler/src/main/resources/jd-gui-duo.bat
@@ -1,1 +1,1 @@
-%~dp0\${jre.path}\bin\java -ea --add-opens java.base/java.net=ALL-UNNAMED --add-opens java.desktop/javax.swing.text=ALL-UNNAMED --add-opens java.prefs/java.util.prefs=ALL-UNNAMED --add-opens java.base/java.lang.ref=ALL-UNNAMED -jar %~dp0\..\..\app\target\jd-gui-duo-app-${project.version}.jar
+${jre.path}\bin\java -ea --add-opens java.base/java.net=ALL-UNNAMED --add-opens java.desktop/javax.swing.text=ALL-UNNAMED --add-opens java.prefs/java.util.prefs=ALL-UNNAMED --add-opens java.base/java.lang.ref=ALL-UNNAMED -cp "lib/*" org.jd.gui.App
