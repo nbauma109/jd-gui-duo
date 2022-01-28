0509ef32090db40958754c1dc68936bb335866cb
nbauma109 <nbauma109@github.com>
2022-01-28 21:10:44 +0100

    --add-opens java.desktop/javax.swing.plaf.basic=ALL-UNNAMED

--- assembler/src/main/resources/jd-gui-duo.bat
+++ assembler/src/main/resources/jd-gui-duo.bat
@@ -1,1 +1,1 @@
-${jre.path}\bin\java -ea --add-opens java.base/java.net=ALL-UNNAMED --add-opens java.desktop/javax.swing.text=ALL-UNNAMED --add-opens java.prefs/java.util.prefs=ALL-UNNAMED --add-opens java.base/java.lang.ref=ALL-UNNAMED -cp "lib/*" org.jd.gui.App
+${jre.path}\bin\java -ea --add-opens java.base/java.net=ALL-UNNAMED --add-opens java.desktop/javax.swing.plaf.basic=ALL-UNNAMED --add-opens java.desktop/javax.swing.text=ALL-UNNAMED --add-opens java.prefs/java.util.prefs=ALL-UNNAMED --add-opens java.base/java.lang.ref=ALL-UNNAMED -cp "lib/*" org.jd.gui.App
