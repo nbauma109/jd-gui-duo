baaa054cdbb772eaf71ee521c8e810d9eb545aed
nbauma109 <nbauma109@github.com>
2022-01-31 15:56:02 +0100

    deleted resources that were moved to jd-core

--- services/src/test/java/org/jd/core/v1/stub/AutoboxingAndUnboxing.java
+++
@@ -1,9 +0,0 @@
-package org.jd.core.v1.stub;
-
-public class  AutoboxingAndUnboxing {
-    void test() {
-        Integer intObj = 10;
-        int i = intObj;
-        System.out.println(i);
-    }
-}
