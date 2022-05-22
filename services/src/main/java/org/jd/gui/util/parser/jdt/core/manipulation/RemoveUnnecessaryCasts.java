5f202e29a60bbd5569d85d6bc10c50926145b959
nbauma109 <nbauma109@github.com>
2022-05-22 18:15:03 +0200

    improve remove unnecessary casts

--- services/src/main/java/org/jd/gui/util/parser/jdt/core/manipulation/RemoveUnnecessaryCasts.java
+++ services/src/main/java/org/jd/gui/util/parser/jdt/core/manipulation/RemoveUnnecessaryCasts.java
@@ -1,14 +1,18 @@
 package org.jd.gui.util.parser.jdt.core.manipulation;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.compiler.IProblem;
+import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
-import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CastExpression;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+import org.eclipse.jdt.internal.corext.dom.ASTNodes;
+import org.eclipse.jdt.internal.corext.fix.UnusedCodeFixCore.RemoveAllCastOperation;
+import org.eclipse.jdt.internal.corext.refactoring.structure.CompilationUnitRewrite;
+import org.eclipse.jdt.internal.ui.text.correction.ProblemLocationCore;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.Document;
-import org.eclipse.text.edits.DeleteEdit;
 import org.eclipse.text.edits.MalformedTreeException;
 import org.eclipse.text.edits.TextEdit;
 import org.jd.core.v1.service.converter.classfiletojavasyntax.util.ExceptionUtil;
@@ -16,8 +20,7 @@ import org.jd.gui.api.model.Container;
 import org.jd.gui.util.parser.jdt.ASTParserFactory;
 
 import java.net.URI;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedHashSet;
 
 public final class RemoveUnnecessaryCasts {
 
@@ -33,29 +36,26 @@ public final class RemoveUnnecessaryCasts {
         Document document = new Document(source);
         ASTParser astParser = ASTParserFactory.getInstanceWithBindings().newASTParser(source.toCharArray(), unitName, jarURI);
         CompilationUnit cu = (CompilationUnit) astParser.createAST(null);
-        ASTRewrite astRewrite = ASTRewrite.create(cu.getAST());
-        TextEdit textEdit = astRewrite.rewriteAST(document, null);
-        Set<Integer> unnecessaryCasts = new HashSet<>();
+        LinkedHashSet<CastExpression> unnecessaryCasts = new LinkedHashSet<>();
         IProblem[] problems = cu.getProblems();
         for (IProblem pb : problems) {
             if (pb.getID() == IProblem.UnnecessaryCast) {
-                unnecessaryCasts.add(pb.getSourceStart());
+                ProblemLocationCore problemLocationCore = new ProblemLocationCore(pb);
+                ASTNode selectedNode = problemLocationCore.getCoveringNode(cu);
+                ASTNode curr = ASTNodes.getUnparenthesedExpression(selectedNode);
+                if (curr instanceof CastExpression) {
+                    unnecessaryCasts.add((CastExpression) curr);
+                }
             }
         }
-        cu.accept(new ASTVisitor() {
-            @Override
-            public boolean visit(CastExpression node) {
-                int start = node.getStartPosition();
-                if (unnecessaryCasts.contains(start)) {
-                    int length = node.getExpression().getStartPosition() - start;
-                    textEdit.addChild(new DeleteEdit(start, length));
-                }
-                return true;
-            }
-        });
+        CompilationUnitRewrite compilationUnitRewrite = new CompilationUnitRewrite(null, cu);
+        RemoveAllCastOperation removeAllCastOperation = new RemoveAllCastOperation(unnecessaryCasts);
         try {
+            removeAllCastOperation.rewriteAST(compilationUnitRewrite, null);
+            ASTRewrite astRewrite = compilationUnitRewrite.getASTRewrite();
+            TextEdit textEdit = astRewrite.rewriteAST(document, null);
             textEdit.apply(document);
-        } catch (MalformedTreeException | BadLocationException e) {
+        } catch (MalformedTreeException | BadLocationException | CoreException e) {
             assert ExceptionUtil.printStackTrace(e);
         }
         return document.get();
