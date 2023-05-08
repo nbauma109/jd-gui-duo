d8862d79b0d724a775f180fb3ddb56680a8fb01e
nbauma109 <nbauma109@github.com>
2023-05-08 10:03:38 +0200

    fix regression in LogPasteHandler - added plain text icon

--- services/src/main/java/org/jd/gui/service/pastehandler/LogPasteHandler.java
+++ services/src/main/java/org/jd/gui/service/pastehandler/LogPasteHandler.java
@@ -9,13 +9,17 @@ package org.jd.gui.service.pastehandler;
 
 import org.jd.gui.api.API;
 import org.jd.gui.spi.PasteHandler;
+import org.jd.gui.util.ImageUtil;
 import org.jd.gui.view.component.LogPage;
 
 import java.net.URI;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import javax.swing.ImageIcon;
+
 public class LogPasteHandler implements PasteHandler { // NO_UCD (unused code)
     protected static final AtomicInteger counter = new AtomicInteger(0);
+    protected static final ImageIcon ICON = new ImageIcon(ImageUtil.getImage("/org/jd/gui/images/file_obj.png"));
 
     @Override
     public boolean accept(Object obj) { return obj instanceof String; }
@@ -25,6 +29,6 @@ public class LogPasteHandler implements PasteHandler { // NO_UCD (unused code)
         String title = "clipboard-" + counter.incrementAndGet() + ".log";
         URI uri = URI.create("memory://" + title);
         String content = obj == null ? "" : obj.toString();
-        api.addPanel(null, title, null, null, new LogPage(api, uri, content));
+        api.addPanel(null, title, () -> ICON, null, new LogPage(api, uri, content));
     }
 }
