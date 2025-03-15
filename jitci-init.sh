7be862474cf6760eeb21753893c10d8d19335b49
nbauma109 <nbauma109@users.noreply.github.com>
2025-03-15 10:54:10 +0100

    upgrade to jdk 21 (#169)
    * upgrade to jdk 21
    
    * use jre-builder 3.0.0
    
    * update jre digest
    
    ---------
    
    Co-authored-by: nbauma109 <nbauma109@github.com>

--- jitci-init.sh
+++ jitci-init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 . ~/.sdkman/bin/sdkman-init.sh
-sdk install java 17.0.11-oracle
-sdk use java 17.0.11-oracle
+sdk install java 21.0.6-oracle
+sdk use java 21.0.6-oracle
 export JAVA_HOME=~/.sdkman/candidates/java/current
 export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
 mvn wrapper:wrapper -Dmaven=${M3_VERSION} --no-transfer-progress
