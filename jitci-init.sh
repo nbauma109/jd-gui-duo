a917d26a01475ba4713b76f28ea79e7a28c75b5e
nbauma109 <nbauma109@users.noreply.github.com>
2025-11-15 13:44:46 +0100

    upgrade to Java 25 (#230)
    Co-authored-by: nbauma109 <nbauma109@github.com>

--- jitci-init.sh
+++ jitci-init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 . ~/.sdkman/bin/sdkman-init.sh
-sdk install java 21.0.6-oracle
-sdk use java 21.0.6-oracle
+sdk install java 25.0.1-oracle
+sdk use java 25.0.1-oracle
 export JAVA_HOME=~/.sdkman/candidates/java/current
 export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
 mvn wrapper:wrapper -Dmaven=${M3_VERSION} --no-transfer-progress
