2fcdf48f9fb97874cda7a539f9558936d7ce1fba
nbauma109 <nbauma109@github.com>
2026-02-06 20:54:59 +0100

    Improve recognition of container tree nodes (BOOT-INF/classes, etc...)

--- services/src/main/java/org/jd/gui/service/treenode/ClassesDirectoryTreeNodeFactoryProvider.java
+++ services/src/main/java/org/jd/gui/service/treenode/ClassesDirectoryTreeNodeFactoryProvider.java
@@ -9,6 +9,9 @@ package org.jd.gui.service.treenode;
 
 import org.jd.gui.util.ImageUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.ImageIcon;
 
 public class ClassesDirectoryTreeNodeFactoryProvider extends DirectoryTreeNodeFactoryProvider {
@@ -17,9 +20,43 @@ public class ClassesDirectoryTreeNodeFactoryProvider extends DirectoryTreeNodeFa
 
     @Override
     public String[] getSelectors() {
-        return appendSelectors("jar:dir:META-INF/versions", "jar:dir:META-INF/versions/5", "jar:dir:META-INF/versions/6", "jar:dir:META-INF/versions/7",
-                "jar:dir:META-INF/versions/8", "jar:dir:META-INF/versions/9", "jar:dir:META-INF/versions/10", "jar:dir:META-INF/versions/11", "jar:dir:META-INF/versions/12",
-                "jar:dir:META-INF/versions/13", "jar:dir:META-INF/versions/14", "war:dir:WEB-INF/classes", "jmod:dir:classes");
+        // We keep the original archive selectors and expand them to cover:
+        // - Java Archive (JAR), Web Application Archive (WAR), Java Module (JMOD)
+        // - Multi-release layouts without needing to update this code every Java release
+        // - Spring Boot executable layout (BOOT-INF/classes)
+        final List<String> selectors = new ArrayList<>();
+
+        // Multi-release (standard)
+        selectors.add("jar:dir:META-INF/versions");
+        addVersionedSelectors(selectors, "jar:dir:META-INF/versions", 5, 255);
+
+        // Spring Boot executable JAR layout
+        selectors.add("jar:dir:BOOT-INF/classes");
+        selectors.add("jar:dir:BOOT-INF/classes/META-INF/versions");
+        addVersionedSelectors(selectors, "jar:dir:BOOT-INF/classes/META-INF/versions", 5, 255);
+
+        // Standard WAR layout, plus the same path when a WAR is treated as a plain JAR
+        selectors.add("war:dir:WEB-INF/classes");
+        selectors.add("jar:dir:WEB-INF/classes");
+
+        // JMOD layout (and a multi-release-like variant under the classes root)
+        selectors.add("jmod:dir:classes");
+        selectors.add("jmod:dir:classes/META-INF/versions");
+        addVersionedSelectors(selectors, "jmod:dir:classes/META-INF/versions", 5, 255);
+
+        // Some packagers place classes under a "classes" directory even in JARs
+        selectors.add("jar:dir:classes");
+
+        return appendSelectors(selectors.toArray(new String[0]));
+    }
+
+    private static void addVersionedSelectors(final List<String> selectors,
+                                             final String baseSelector,
+                                             final int minVersion,
+                                             final int maxVersion) {
+        for (int version = minVersion; version <= maxVersion; version++) {
+            selectors.add(baseSelector + "/" + version);
+        }
     }
 
     @Override
