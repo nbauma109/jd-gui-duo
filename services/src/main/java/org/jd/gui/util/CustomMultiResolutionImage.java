970d1cf6b44008e48c5e0b7e13e0f93dbe2abf75
nicolasbaumann <nicolasbaumann@localhost>
2026-02-28 22:15:45 +0100

    Scale image icons

--- services/src/main/java/org/jd/gui/util/CustomMultiResolutionImage.java
+++ services/src/main/java/org/jd/gui/util/CustomMultiResolutionImage.java
@@ -1,6 +1,8 @@
 package org.jd.gui.util;
 
 import java.awt.Image;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BaseMultiResolutionImage;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -33,8 +35,16 @@ public class CustomMultiResolutionImage extends BaseMultiResolutionImage {
         Image[] images = new Image[imageCount + 1];
         images[0] = baseImage;
         for (int i = 1; i < images.length; i++) {
-            images[i] = new BufferedImage(baseWidth + 2 * i, baseHeight + 2 * i, BufferedImage.TYPE_INT_ARGB);
-            images[i].getGraphics().drawImage(baseImage, i, i, null);
+            int targetWidth = baseWidth + 2 * i;
+            int targetHeight = baseHeight + 2 * i;
+            BufferedImage scaledImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2d = scaledImage.createGraphics();
+            g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.drawImage(baseImage, 0, 0, targetWidth, targetHeight, null);
+            g2d.dispose();
+            images[i] = scaledImage;
         }
         return images;
     }
