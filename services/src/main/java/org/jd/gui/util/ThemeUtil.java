43b9117963bd65a6b6210eb134ff12d329518ca5
nicolasbaumann <nicolasbaumann@localhost>
2026-02-28 19:21:43 +0100

    Fix bug where fonts are not applied to new editors

--- services/src/main/java/org/jd/gui/util/ThemeUtil.java
+++ services/src/main/java/org/jd/gui/util/ThemeUtil.java
@@ -1,9 +1,16 @@
+/*
+ * Copyright (c) 2025-2026 Nicolas Baumann (@nbauma109).
+ * This project is distributed under the GPLv3 license.
+ * This is a Copyleft license that gives the user the right to use,
+ * copy and modify the code freely for non-commercial purposes.
+ */
 package org.jd.gui.util;
 
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.Theme;
 import org.jd.core.v1.service.converter.classfiletojavasyntax.util.ExceptionUtil;
 import org.jd.gui.api.API;
+import org.jd.gui.util.decompiler.GuiPreferences;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,19 +21,19 @@ import javax.swing.UIManager;
 
 public class ThemeUtil {
 
-	private ThemeUtil() {
-	}
+    private ThemeUtil() {
+    }
 
-	public static RSyntaxTextArea applyTheme(API api, RSyntaxTextArea rSyntaxTextArea) {
+    public static RSyntaxTextArea applyTheme(API api, RSyntaxTextArea rSyntaxTextArea) {
         return applyTheme(api.getPreferences(), rSyntaxTextArea);
-	}
+    }
 
-	public static RSyntaxTextArea applyTheme(Map<String, String> preferences, RSyntaxTextArea rSyntaxTextArea) {
-		String themeResource;
-		if (Boolean.parseBoolean(preferences.getOrDefault("UIMainWindowPreferencesProvider.darkMode", "false"))) {
-        	themeResource = "rsyntaxtextarea/themes/dark.xml";
+    public static RSyntaxTextArea applyTheme(Map<String, String> preferences, RSyntaxTextArea rSyntaxTextArea) {
+        String themeResource;
+        if (Boolean.parseBoolean(preferences.getOrDefault("UIMainWindowPreferencesProvider.darkMode", "false"))) {
+            themeResource = "rsyntaxtextarea/themes/dark.xml";
         } else {
-        	themeResource = "rsyntaxtextarea/themes/eclipse.xml";
+            themeResource = "rsyntaxtextarea/themes/eclipse.xml";
         }
         try (InputStream resourceAsStream = RSyntaxTextArea.class.getClassLoader().getResourceAsStream(themeResource)) {
             Theme theme = Theme.load(resourceAsStream);
@@ -34,10 +41,18 @@ public class ThemeUtil {
         } catch (IOException e) {
             assert ExceptionUtil.printStackTrace(e);
         }
-		return rSyntaxTextArea;
-	}
+        String fontSize = preferences.get(GuiPreferences.FONT_SIZE_KEY);
+        if (fontSize != null) {
+            try {
+                rSyntaxTextArea.setFont(rSyntaxTextArea.getFont().deriveFont(Float.parseFloat(fontSize)));
+            } catch (Exception e) {
+                assert ExceptionUtil.printStackTrace(e);
+            }
+        }
+        return rSyntaxTextArea;
+    }
 
-	public static String getDefaultLookAndFeel() {
-		return Optional.ofNullable(System.getProperty("swing.defaultlaf")).orElseGet(UIManager::getSystemLookAndFeelClassName);
-	}
+    public static String getDefaultLookAndFeel() {
+        return Optional.ofNullable(System.getProperty("swing.defaultlaf")).orElseGet(UIManager::getSystemLookAndFeelClassName);
+    }
 }
